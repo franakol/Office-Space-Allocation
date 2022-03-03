d0ba35098e3ddbe2a42dbdade9d3a4cb3c0fdcb7
Francis Akol <akolfrancis40@gmail.com>
2022-03-03 22:53:37 +0300

    bug fixes

--- app/staff.py
+++
@@ -1,7 +0,0 @@
-from app.person import Person
-
-class Staff(Person):
-    """this is the Staff class"""
-
-    def __init__(self, name, sex, age):
-        super(Staff, self).__init__(name,sex, age)
